cc5cc187f91132d32cc640d33e8dd3a8a0c77448
Josh Petry <josh.petry@outlook.com>
2019-01-29 06:42:12 -0500

    Final blog application

--- js/app.js
+++ js/app.js
@@ -1,12 +1,16 @@
 $('#flashMessage')
-    .hide()
+.hide()
+
+
+$('#previewButton').click(function(){
+    const title = $('#blogTitleInput').val();
+    const content = $('#blogContentInput').val();
+    
+    $('#blogTitlePreview').html(title);
+    $('#blogContentPreview').html(content);
+    
+    $('#flashMessage')
     .slideDown(1000)
     .delay(3000)
     .slideUp();
-
-const title = "My First Blog Post";
-const content = "This is my <strong>first</strong> post.";
-
-$('#blogTitlePreview').html(title);
-$('#blogContentPreview').html(content);
-
+});
