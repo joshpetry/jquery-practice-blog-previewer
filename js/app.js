8e00f7acb5bf9ea8b90ea4ee5cc63aec7edfd56d
Josh Petry <josh.petry@outlook.com>
2019-01-28 20:03:56 -0500

    Add sample text into blog preview

--- js/app.js
+++ js/app.js
@@ -3,3 +3,10 @@ $('#flashMessage')
     .slideDown(1000)
     .delay(3000)
     .slideUp();
+
+const title = "My First Blog Post";
+const content = "This is my <strong>first</strong> post.";
+
+$('#blogTitlePreview').html(title);
+$('#blogContentPreview').html(content);
+
